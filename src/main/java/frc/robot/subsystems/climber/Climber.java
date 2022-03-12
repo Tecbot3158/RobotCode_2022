fda389eaaf6ede48cae88e6ffdaa560bf2254d42
Hector Canizales <heror0484@protonmail.com>
2022-03-11 19:52:31 -0600

    reformat Climber and add PID methods for SparkMax

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -10,55 +10,110 @@ package frc.robot.subsystems.climber;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Robot;
 import frc.robot.RobotMap;
-import frc.robot.resources.Math;
-import frc.robot.resources.*;
+import frc.robot.resources.RobotConfigurator;
+import frc.robot.resources.TecbotMotorList;
 
 public class Climber extends SubsystemBase {
 
     private final DoubleSolenoid pistonHanger;
     private final TecbotMotorList ropeController;
 
-   public Climber() {
+    private SparkMaxPIDController ropePIDController0;
+    private RelativeEncoder ropeEncoder0;
 
-       ropeController = RobotConfigurator.buildMotorList(RobotMap.CLIMBER_MOTOR_PORTS,  RobotMap.CLIMBER_INVERTED_MOTORS, RobotMap.CLIMBER_MOTOR_TYPES );
-       pistonHanger = RobotConfigurator.buildDoubleSolenoid(RobotMap.CLIMBER_SOLENOID_PORTS);
+    private SparkMaxPIDController ropePIDController1;
+    private RelativeEncoder ropeEncoder1;
 
-   }
+    private double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
+    private double ropePIDController0TargetRPM;
 
-   public void setPistonExtend(){
-       pistonHanger.set(RobotMap.CLIMBER_SOLENOID_EXTENDED_POSITION );
-   }
 
-   public void setPistonRetract(){
-       pistonHanger.set(RobotMap.CLIMBER_SOLENOID_RETRACTED_POSITION );
-   }
+    public Climber() {
 
-   public void setPistonRaw(DoubleSolenoid.Value state){
-       pistonHanger.set(state);
-   }
+        ropeController = RobotConfigurator.buildMotorList(RobotMap.CLIMBER_MOTOR_PORTS, RobotMap.CLIMBER_INVERTED_MOTORS, RobotMap.CLIMBER_MOTOR_TYPES);
+        pistonHanger = RobotConfigurator.buildDoubleSolenoid(RobotMap.CLIMBER_SOLENOID_PORTS);
 
-   public void setRopeControllerRaw(double speed){
-       ropeController.setAll(speed);
-   }
 
-   public TecbotMotorList getRopeControllerList(){
-       return ropeController;
-   }
+        //TODO disable dragon fly in command for climber! Always!
+        //TODO Dragon fly cannot be extended!!
 
-   /**
-   Returns a CAN Spark Max {@link RelativeEncoder}
-    @param index index of the MotorList to retrieve the encoder from.
-    */
-   public RelativeEncoder getRopeControllerEncoder(int index){
-       return ropeController.getMotors().get(index).getCANSparkMax().getEncoder();
-   }
+        initPID();
 
+    }
+
+    public void initPID() {
+        ropePIDController0 = ropeController.getMotors().get(0).getCANSparkMax().getPIDController();
+        ropeEncoder0 = ropeController.getMotors().get(0).getCANSparkMax().getEncoder();
+
+        ropePIDController1 = ropeController.getMotors().get(1).getCANSparkMax().getPIDController();
+        ropeEncoder1 = ropeController.getMotors().get(1).getCANSparkMax().getEncoder();
+
+        kP = RobotMap.CLIMBER_PID_kP;
+        kI = RobotMap.CLIMBER_PID_kI;
+        kD = RobotMap.CLIMBER_PID_kD;
+        kIz = RobotMap.CLIMBER_PID_kIz;
+        kFF = RobotMap.CLIMBER_PID_kFF;
+        kMaxOutput = RobotMap.CLIMBER_PID_kMaxOutput;
+        kMinOutput = RobotMap.CLIMBER_PID_kMinOutput;
+        maxRPM = RobotMap.CLIMBER_PID_kMaxRPM;
+
+        ropePIDController0TargetRPM = RobotMap.CLIMBER_PID_Target;
+
+        ropeControllerSetPIDValues(ropePIDController0);
+
+        ropeControllerSetPIDValues(ropePIDController1);
+
+
+    }
+
+    private void ropeControllerSetPIDValues(SparkMaxPIDController pidController) {
+        pidController.setP(kP);
+        pidController.setI(kI);
+        pidController.setD(kD);
+        pidController.setIZone(kIz);
+        pidController.setFF(kFF);
+        pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    }
+
+    public void runPID(SparkMaxPIDController pidController) {
+        double setPoint = ropePIDController0TargetRPM;
+        pidController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
+
+    }
+
+    public void setPistonExtend() {
+        pistonHanger.set(RobotMap.CLIMBER_SOLENOID_EXTENDED_POSITION);
+    }
+
+    public void setPistonRetract() {
+        pistonHanger.set(RobotMap.CLIMBER_SOLENOID_RETRACTED_POSITION);
+    }
+
+    public void setPistonRaw(DoubleSolenoid.Value state) {
+        pistonHanger.set(state);
+    }
+
+    public void setRopeControllerRaw(double speed) {
+        ropeController.setAll(speed);
+    }
+
+    public TecbotMotorList getRopeControllerList() {
+        return ropeController;
+    }
+
+    /**
+     * Returns a CAN Spark Max {@link RelativeEncoder}
+     *
+     * @param index index of the MotorList to retrieve the encoder from.
+     */
+    public RelativeEncoder getRopeControllerEncoder(int index) {
+        return ropeController.getMotors().get(index).getCANSparkMax().getEncoder();
+    }
 
 
 }
