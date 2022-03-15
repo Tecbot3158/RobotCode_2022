d13e61c3b18b7b4cc625b2ce136135969f55f57e
Hector Canizales <heror0484@protonmail.com>
2022-03-15 15:34:22 -0600

    comment command

--- src/main/java/frc/robot/commands/intake/IntakeAbsorbExtendThenRetractStop.java
+++ src/main/java/frc/robot/commands/intake/IntakeAbsorbExtendThenRetractStop.java
@@ -4,14 +4,4 @@ import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.Robot;
 import frc.robot.subsystems.intake.Intake;
 
-public class IntakeAbsorbExtendThenRetractStop extends StartEndCommand {
-
-//   public IntakeAbsorbExtendThenRetractStop(){
-//       Intake intake = Robot.getRobotContainer().getIntake();
-//       super( new IntakeAbsorbAndExtend(), new IntakeStopAndRetract(), null );
-//
-//   }
-//
-
-
-}
+// public class IntakeAbsorbExtendThenRetractStop extends StartEndCommand { //   public IntakeAbsorbExtendThenRetractStop(){ //       Intake intake = Robot.getRobotContainer().getIntake(); //       super( new IntakeAbsorbAndExtend(), new IntakeStopAndRetract(), null ); // //   } // }
