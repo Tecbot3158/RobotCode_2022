108bb85c74b0dfa854ca9c23ad59dfa34cbe5433
Hector Canizales <heror0484@protonmail.com>
2022-03-15 19:49:31 -0600

    add step control with increment multipliers.

---
+++ src/main/java/frc/robot/resources/StepControl.java
@@ -0,0 +1,52 @@
+package frc.robot.resources;
+
+public class StepControl {
+
+    double currentPosition = 0;
+    double startPosition = 0;
+    double output = 0;
+
+
+    double kTarget = 0;
+
+    double kMinimumAbsoluteOutput = 0.09;
+
+    double incrementMultiplier = 1;
+
+    double currentOutput = 0;
+
+
+    public StepControl ( double kMinimumAbsoluteOutput, double kTarget, double kCurrentPosition,
+                         double kIncrementMultiplier ){
+        this.kTarget = kTarget;
+        this.kMinimumAbsoluteOutput = kMinimumAbsoluteOutput;
+
+        this.startPosition = kCurrentPosition;
+        currentPosition = kCurrentPosition;
+
+        this.incrementMultiplier = kIncrementMultiplier;
+
+    }
+
+    public double setCurrentPosition( double pos ){
+        this.currentPosition = pos;
+        return pos;
+    }
+
+    public double getOutput( double currentPosition ) {
+        this.currentPosition = currentPosition;
+        double proportion = ( ( kTarget - currentPosition) ) * incrementMultiplier;
+        int sign = proportion >= 0 ? 1: -1;
+
+        proportion = sign  * Math.clamp(Math.abs(proportion), kMinimumAbsoluteOutput, 1);
+
+        currentOutput += proportion;
+
+        return Math.clamp(currentOutput, -1, 1) ;
+
+    }
+
+    public double getCurrentPosition(){
+        return this.currentPosition;
+    }
+}
