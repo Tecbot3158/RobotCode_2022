fb6f7a9f6af1353f4a8a4f108ceec07f17205c1c
Hector Canizales <heror0484@protonmail.com>
2022-03-19 13:07:25 -0600

    add debug() method to Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.chassis.DefaultDrive;
 import frc.robot.commands.chassis.drivingModes.ChassisSetSpeed;
+import frc.robot.resources.TecbotConstants;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -139,5 +140,11 @@ public class Robot extends TimedRobot {
 
     public static RobotContainer getRobotContainer() {
         return robotContainer;
+    }
+
+    public static void debug (String value) {
+        if ( TecbotConstants.DEBUG_ENABLED )
+            System.out.println( value );
+
     }
 }
