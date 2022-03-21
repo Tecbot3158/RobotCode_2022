183246d55d34767471d1518654647c4d3c903155
Hector Canizales <heror0484@protonmail.com>
2022-03-21 13:18:07 -0600

    add 180 deg rotation to five ball autonomous

--- src/main/java/frc/robot/commands/chassis/autonomous/fullAutos/FiveBallsAuto.java
+++ src/main/java/frc/robot/commands/chassis/autonomous/fullAutos/FiveBallsAuto.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.chassis.autonomous.fullAutos;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.commands.turret.MoveTurretToAngle;
 import frc.robot.resources.splines.PieceWiseSpline;
 
 public class FiveBallsAuto extends ParallelCommandGroup {
@@ -8,6 +9,7 @@ public class FiveBallsAuto extends ParallelCommandGroup {
     public FiveBallsAuto(PieceWiseSpline fiveBallAutoPath) {
 
         super(
+                new MoveTurretToAngle(-180),
                 new FiveBallsPath(fiveBallAutoPath),
                 new EverythingToShoot());
 
