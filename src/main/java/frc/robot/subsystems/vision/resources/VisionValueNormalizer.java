1c0229f34fe96eb90c01969fa6ff36693e07a8fe
Hector Canizales <heror0484@protonmail.com>
2022-03-22 21:35:21 -0600

    add a value normalizer for vision processing

--- src/main/java/frc/robot/subsystems/vision/resources/VisionValueNormalizer.java
+++ src/main/java/frc/robot/subsystems/vision/resources/VisionValueNormalizer.java
@@ -4,31 +4,79 @@
 
 package frc.robot.subsystems.vision.resources;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
-import frc.robot.RobotMap;
+import frc.robot.resources.Math;
+import frc.robot.subsystems.vision.TecbotCamera;
 import org.photonvision.PhotonCamera;
-import org.photonvision.targeting.PhotonPipelineResult;
 
-public class VisionValueNormalizer extends SubsystemBase {
+public class VisionValueNormalizer {
 
     PhotonCamera photonCamera;
+    TecbotCamera tecbotCamera;
+
+    int count = 0;
+    int maxCount = 20;
+
+    double sum = 0;
+    double averageArea = 0;
+
+    double minimumArea;
+    double maximumArea;
+    double range;
 
     /**
      * Creates a new Tecbot camera.
      * <p></p>
      * It uses Photonvision with PhotonLib to retrieve the values
      * from the Network Tables.
-     *
+     * <p>
      * It allows to easily retrieve target values ranging from <code>-1 to 1</code>
      */
-    public VisionValueNormalizer() {
+    public VisionValueNormalizer(double minArea, double maxArea) {
 
-        photonCamera = Robot.getRobotContainer().getTecbotCamera().getPhotonCamera();
+        this.minimumArea = minArea;
+        this.maximumArea = maxArea;
 
+        this.range = this.maximumArea - this.minimumArea;
 
+        tecbotCamera = Robot.getRobotContainer().getTecbotCamera();
+        photonCamera = tecbotCamera.getPhotonCamera();
+
+    }
+
+    public void executeAverageArea() {
+        if (count == maxCount) {
+            averageArea = sum / count;
+            count = 0;
+            sum = 0;
+            return;
+        }
+
+        ++count;
+
+        tecbotCamera.update();
+        double area = tecbotCamera.getArea();
+        sum += area;
+        SmartDashboard.putNumber("LIME: area ", area);
 
 
     }
+
+    public double getAverageArea() {
+
+        return averageArea;
+
+    }
+
+    /**
+     * @return average area from 0 to 1.
+     */
+    public double getNormalizedDistance() {
+        double actualAreaDelta = averageArea - minimumArea;
+        return 1 - Math.clamp( actualAreaDelta / range, 0, 1) ;
+
+    }
+
 
 }
